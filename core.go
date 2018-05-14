52383e3e3f82016a63e35d39bb2587c1bc342c60
dlt <dlt@localhost.localdomain>
2018-05-15 01:16:48 +0700

    update dev

--- core.go
+++ core.go
@@ -48,7 +48,7 @@ func (nc *RConn) publish(data []byte) error {
 		log.Errorf("can't publish channal close", nc.host)
 		return ErrConnectionClosed
 	}
-	log.Infof("publishing %v", nc.status)
+	log.Infof("publishing X1 %v", nc.status)
 	nc.mu.Lock()
 	defer nc.mu.Unlock()
 	if nc.isReconnecting() {
@@ -59,7 +59,7 @@ func (nc *RConn) publish(data []byte) error {
 			return ErrReconnectBufExceeded
 		}
 	}
-	log.Infof("publish %v", nc.status)
+	log.Infof("publish X2 %v", nc.status)
 	l, err := nc.bw.Write(data)
 	if err != nil {
 		return err
