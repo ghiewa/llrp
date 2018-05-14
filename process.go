8f4b205ed991d00ee0f7185c695920d057be4099
dlt <dlt@localhost.localdomain>
2018-05-14 16:56:30 +0700

    add network recovery

--- process.go
+++ process.go
@@ -197,6 +197,7 @@ func (nc *RConn) process(b []byte, len_data int) error {
 	}
 
 	if vaild {
+		log.Infof("reports ", len(reports))
 		nc.mu.Lock()
 		nc.sendReport(len_data, reports)
 		nc.mu.Unlock()
