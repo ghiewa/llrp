b6869ef1ba217e59f715c5bf561a1f12d550d80d
dlt <dlt@localhost.localdomain>
2018-05-26 22:51:25 +0700

    debug pos 36

--- ro.go
+++ ro.go
@@ -104,9 +104,9 @@ func PeriodicTriggerValue(UTCTime uint64, offset uint32, period uint32) []interf
 // option gpi trigger for rospec start/stop trigger
 func GPITriggerValue(GPIPortNum uint16, GPIEvent bool, Timeout uint32) []interface{} {
 	l := 4 + 2 + 1 + 4
-	ev := uint8(0) // disable
-	if GPIEvent {
-		ev = 1
+	ev := uint8(80) // disable
+	if !GPIEvent {
+		ev = 0
 	}
 	return []interface{}{
 		uint16(P_GPITriggerValue),
